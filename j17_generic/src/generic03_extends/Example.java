5488471edc1106937911c839e6fd3fb0289336c9
DAHAN7 <ekgks4843@gmail.com>
2024-05-30 15:50:39 +0900

    first commit

--- j17_generic/src/generic03_extends/Example.java
+++ j17_generic/src/generic03_extends/Example.java
@@ -6,6 +6,12 @@ public class Example {
 		Parent<String, Integer> p1 = new Parent<>();
 		p1.setKind("Car");
 		p1.setModel(520);
+		
+		Child<String,Integer,String> child = new Child<>();
+		child.setKind("Car");
+		child.setModel(220);
+		child.setCompany("BMW");
+		System.out.println(child);
 	}
 
 }
